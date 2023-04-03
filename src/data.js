c7bf65b0b97ea5a1f0f046707238171d531f562f
Muhammad Daffa Ferdiansyah <daffaferdiansyah48@gmail.com>
2023-04-04 05:38:15 +0700

    add data

---
+++ src/data.js
@@ -0,0 +1,18 @@
+import img1 from "./assets/images/1.webp";
+import img2 from "./assets/images/2.webp";
+
+const data = [
+    {
+        "nama": "Nasi Goreng Kebuli Sapi",
+        "gambar": img1,
+        "kategori": "Nasi Goreng Kebuli",
+        "harga": 24000
+    },
+    {
+        "nama": "Nasi Goreng Kebuli Ayam",
+        "gambar": img2,
+        "kategori": "Nasi Goreng Kebuli",
+        "harga": 24000
+    },
+];
+export default data;
